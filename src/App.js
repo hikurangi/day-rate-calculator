24d86f41321f2fd277ebb1eccba779caa536c0b2
hikurangi <hikurangi@gmail.com>
2016-11-07 01:22:28 +1300

    experimenting with material ui components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import logo from './logo.svg';
 import './App.css';
+
+import Calculator from './Calculator'
+
 
 class App extends Component {
   render() {
@@ -8,11 +13,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Contractor Day Rate Calculator</h2>
+          <p>by Hikurangi Schaverien-Kaa with information from Jody at Enspiral Dev Academy</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MuiThemeProvider>
+          <Calculator />
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ src/Calculator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const Calculator = () => (
+  <RaisedButton className="mui-components" label="Default" />
+);
+
+export default Calculator
