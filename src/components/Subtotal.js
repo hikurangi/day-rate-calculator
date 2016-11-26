37126e3feffc03fa99f4ace88e58fc8c4d00eb90
hikurangi <hikurangi@gmail.com>
2016-11-26 20:14:50 +1300

    subtotal => sfc

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-class Subtotal extends Component {
-  render() {
-    return (
-      <TableRow>
-        <TableRowColumn><h3>Total</h3></TableRowColumn>
-        <TableRowColumn>{ this.props.subtotal ? '$ ' + (this.props.subtotal).toFixed(2) : null }</TableRowColumn>
-      </TableRow>
-    )
-  }
+const Subtotal = ({ subtotal }) => {
+  return (
+    <TableRow>
+      <TableRowColumn><h3>Total</h3></TableRowColumn>
+      <TableRowColumn>{ subtotal ? '$ ' + (subtotal).toFixed(2) : null }</TableRowColumn>
+    </TableRow>
+  )
 }
 
 export default Subtotal
