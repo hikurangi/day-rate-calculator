3f9ec77157fd0f5b536427be4483e605874ee30a
hikurangi <hikurangi@gmail.com>
2016-11-26 20:10:33 +1300

    Depreciation and Header => sfcs

--- src/components/Depreciation.js
+++ src/components/Depreciation.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 import TextField from 'material-ui/TextField'
 
-class Depreciation extends Component {
-  render() {
-    return (
-      <TableRow>
-        <TableRowColumn>
-          <h3>Depreciation</h3>
-          The cost of your laptop:
-          <TextField
-            onChange={this.props.handleProp('laptopValue')}
-          />
-          <br/>
-          Client pays a 50% share
-          <br/>
-          Your <a href="http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf" target="blank">FTE </a>
-          <TextField
-            onChange={this.props.handleProp('fte')}
-          />
-        </TableRowColumn>
-        <TableRowColumn>
-          { this.props.depreciation && ( this.props.depreciation !== Infinity ) ? '$ ' + (this.props.depreciation * 12).toFixed(2) : null }</TableRowColumn>
-      </TableRow>
-    )
-  }
+const Depreciation = ({ handleProp, depreciation }) => {
+  return (
+    <TableRow>
+      <TableRowColumn>
+        <h3>Depreciation</h3>
+        The cost of your laptop:
+        <TextField
+          onChange={handleProp('laptopValue')}
+        />
+        <br/>
+        Client pays a 50% share
+        <br/>
+        Your <a href="http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf" target="blank">FTE </a>
+        <TextField
+          onChange={handleProp('fte')}
+        />
+      </TableRowColumn>
+      <TableRowColumn>
+        { depreciation && ( depreciation !== Infinity ) ? '$ ' + ( depreciation * 12 ).toFixed(2) : null }</TableRowColumn>
+    </TableRow>
+  )
 }
 
 export default Depreciation
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from '../assets/logo.svg'
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Contractor Day Rate Calculator</h2>
-        <h3><em>accurate for {this.props.thisYear}</em></h3>
-        <p>Built by Hikurangi Schaverien-Kaa with information from Jody at Enspiral Dev Academy</p>
-      </div>
-    )
-  }
+const Header = ({ thisYear }) => {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Contractor Day Rate Calculator</h2>
+      <h3><em>accurate for { thisYear }</em></h3>
+      <p>Built by Hikurangi Schaverien-Kaa with information from Jody at Enspiral Dev Academy</p>
+    </div>
+  )
 }
 
 export default Header
