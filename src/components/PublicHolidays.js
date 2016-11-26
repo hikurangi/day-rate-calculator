20666962d50cc792d2dc1ba8170cfe08507bd85c
hikurangi <hikurangi@gmail.com>
2016-11-26 20:11:58 +1300

    PublicHolidays => sfc

--- src/components/PublicHolidays.js
+++ src/components/PublicHolidays.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-class PublicHolidays extends Component {
-  render() {
-    return (
-      <TableRow>
-        <TableRowColumn>Public holidays (this is based on 2016)	</TableRowColumn>
-        <TableRowColumn>12</TableRowColumn>
-      </TableRow>
-    )
-  }
+const PublicHolidays = ({ publicHolidays }) => {
+  return (
+    <TableRow>
+      <TableRowColumn>Public holidays (this is based on 2016)	</TableRowColumn>
+      <TableRowColumn>12</TableRowColumn>
+    </TableRow>
+  )
 }
 
 export default PublicHolidays
