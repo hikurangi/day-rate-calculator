2c75a95233254f437b84640753c77b08daf1742e
hikurangi <hikurangi@gmail.com>
2016-11-26 20:02:59 +1300

    refactor Kiwisaver as s.f.c.

--- src/components/Kiwisaver.js
+++ src/components/Kiwisaver.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-class Kiwisaver extends Component {
-  render() {
-    return (
-      <TableRow>
-        <TableRowColumn>
-          <h3>3% Kiwisaver Employer minimum contribution</h3>
-        </TableRowColumn>
-        <TableRowColumn>
-          { this.props.kiwisaver ? '$ ' + (this.props.kiwisaver * 12).toFixed(2) : null }
-        </TableRowColumn> {/* should actually show lower values down to three cents */}
-      </TableRow>
-    )
-  }
+const Kiwisaver = ({kiwisaver}) => {
+  return (
+    <TableRow>
+      <TableRowColumn>
+        <h3>3% Kiwisaver Employer minimum contribution</h3>
+      </TableRowColumn>
+      <TableRowColumn>
+        { kiwisaver ? '$ ' + (kiwisaver * 12).toFixed(2) : null }
+      </TableRowColumn> {/* should actually show lower values down to three cents */}
+    </TableRow>
+  )
 }
 
 export default Kiwisaver
