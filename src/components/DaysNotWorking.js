89e91fc8979677428589d0a0548281cf5d111d28
hikurangi <hikurangi@gmail.com>
2016-11-26 20:06:49 +1300

    missed a arrow

--- src/components/DaysNotWorking.js
+++ src/components/DaysNotWorking.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-const DaysNotWorking = ({ daysNotWorking }) {
+const DaysNotWorking = ({ daysNotWorking }) => {
   return (
     <TableRow>
       <TableRowColumn>Total days you are NOT working</TableRowColumn>
