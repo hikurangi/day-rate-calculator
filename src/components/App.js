ff12d5d1f63cd6568b0e52a8d89f72078e3c5c5f
hikurangi <hikurangi@gmail.com>
2016-11-27 14:13:04 +1300

    app => sfc

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
@@ -6,16 +6,14 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 import '../App.css'
 import Calculator from './Calculator'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-          <Calculator />
-        </MuiThemeProvider>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Calculator />
+      </MuiThemeProvider>
+    </div>
+  )
 }
 
 export default App;
