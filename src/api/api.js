a3e374c9788b6839b590e1f28d8387fc10419bb3
hikurangi <hikurangi@gmail.com>
2016-12-02 23:12:40 +1300

    not actually sure whats changed here... nothing major

--- src/api/api.js
+++ src/api/api.js
@@ -20,7 +20,6 @@ path: '/public/v1/'
 
 // publicHolidays retrieves the holidays in a given year
 const publicHolidays = function (year, country) {
-  //pretty sure that this will be to api object below
   this.options.path += `public-holidays?countryCode=${country}&startDate=${year}-01-01&endDate=${year}-12-31`
   console.log('year in PublicHolidays', year)
 
