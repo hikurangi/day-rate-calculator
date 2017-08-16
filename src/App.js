e1e59342a976bf162233d9ffde48215cacf96d15
hikurangi <hikurangi@gmail.com>
2017-08-16 15:46:29 +1200

    reset

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import './App.css'
-import Calculator from './components/Calculator'
-
-const App = () => {
-  return (
-    <div className="App">
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <Calculator />
-      </MuiThemeProvider>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
