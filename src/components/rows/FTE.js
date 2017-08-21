70479d5f2f351cbd50379feda30dbf80c9aeb2b4
hikurangi <hikurangi@gmail.com>
2017-08-21 23:09:47 +1200

    tidying

--- src/components/rows/FTE.js
+++ src/components/rows/FTE.js
@@ -2,7 +2,7 @@ const FTE = ({ handleChange }) => {
   return (
     <TableRowColumn>
       <h3>
-        Your <a href="http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf" target="blank"> FTE </a>
+        Your <a href="http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf" target="blank">FTE</a>:
       </h3>
     </TableRowColumn>
     <TableRowColumn>
