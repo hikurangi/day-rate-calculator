f55b7b994e558b82bf5653b4dee268941009714a
hikurangi <hikurangi@gmail.com>
2017-08-22 00:36:09 +1200

    definite anti-pattern

--- src/components/rows/presentation/Subtotal.js
+++ src/components/rows/presentation/Subtotal.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
 const Subtotal = ({ style, salary, depreciation, cellphone }) => {
-  const subtotal = () => [...arguments].reduce((arg, acc) => {
+  const subtotal = (...args) => args.reduce((arg, acc) => {
     console.log({arg, acc});
     return acc + ((arg === undefined) || (arg === null) || (arg === 0)) ? 0 : arg
   }, 0)
@@ -12,7 +12,7 @@ const Subtotal = ({ style, salary, depreciation, cellphone }) => {
       <TableRowColumn><h3>Subtotal</h3></TableRowColumn>
       <TableRowColumn>
         <p style={style.outputNumbers}>
-          { subtotal(salary, salary * 0.03, depreciation, cellphone * 12) !== 0 ? '$ ' + subtotal(salary, salary * 0.03, depreciation, cellphone * 12) : '' }
+          { '$ ' + subtotal(salary, salary * 0.03, depreciation, cellphone * 12) }
         </p>
       </TableRowColumn>
     </TableRow>
