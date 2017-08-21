737e87b30c6f8754a972566d909fd703439caf8d
hikurangi <hikurangi@gmail.com>
2017-08-21 23:30:53 +1200

    style, separating input from presentation

---
+++ src/components/rows/presentation/Depreciation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TableRow, TableRowColumn } from 'material-ui/Table'
+
+const Depreciation = ({ handleChange, style, laptop, fte }) => {
+  return (
+    <TableRow>
+      <TableRowColumn>
+        <h3>Depreciation</h3>
+      </TableRowColumn>
+      <TableRowColumn>
+        <p className={style.outputNumbers}>{laptop && fte ? '$ ' + ((laptop/3)/(fte*2)).toFixed(2) : ''}</p>
+      </TableRowColumn>
+    </TableRow>
+  )
+}
+
+export default Depreciation
