d5e3aecf5d198594a51acaf0e8f2705c0444d699
hikurangi <hikurangi@gmail.com>
2017-08-23 12:29:57 +1200

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { TableRow, TableHeaderColumn } from 'material-ui/Table'
 
 import logo from '../assets/logo.svg'
 
 const Header = ({ thisYear }) => {
   return (
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Contractor Day Rate Calculator</h2>
-      <h3><em>accurate for { thisYear }</em></h3>
-      <p>Built by Hikurangi Schaverien-Kaa with information from Jody at Enspiral Dev Academy</p>
-    </div>
+    <TableRow className="App-header">
+      <TableHeaderColumn>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Contractor Day Rate Calculator</h2>
+        <h3><em>accurate for { thisYear }</em></h3>
+        <p>
+          Built by Hikurangi Schaverien-Kaa with information from Jody at Enspiral Dev Academy
+        </p>
+      </TableHeaderColumn>
+    </TableRow>
   )
 }
 
