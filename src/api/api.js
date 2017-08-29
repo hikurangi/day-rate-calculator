cc3ff533c2734c56577b9b9a320c3072b8c21cc1
hikurangi <hikurangi@gmail.com>
2017-08-29 13:30:46 +1200

    add basic content from google sample

--- src/api/api.js
+++ src/api/api.js
@@ -1,50 +1,109 @@
-const http = require ('http')
-// Originally used the ASB public holidays API https://developer.asb.co.nz/documentation/public-holidays
+var CLIENT_ID = '<YOUR_CLIENT_ID>';
 
-// Now using Enrico Service http://kayaposoft.com/enrico/
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
-// var req = http.request({
-//     withCredentials: false
-// }, function(res) {
-//     //...
-// });
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+//
+// var authorizeButton = document.getElementById('authorize-button');
+// var signoutButton = document.getElementById('signout-button');
 
-function publicHolidays(year, country) {
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
 
-  const options = {
-    host: 'kayaposoft.com',
-    // port: 80, // default
-    path: `/enrico/json/v1.0?action=getPublicHolidaysForYear&year=${year}&country=${country}&region=`,
-    // method: 'GET', // default
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'withCredentials': false,
-      'Access-Control-Allow-Origin': '*'
+/**
+*  Initializes the API client library and sets up sign-in state
+*  listeners.
+*/
+function initClient() {
+  gapi.client.init({discoveryDocs: DISCOVERY_DOCS, clientId: CLIENT_ID, scope: SCOPES}).then(function() {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  });
+}
+
+/**
+*  Called when the signed in status changes, to update the UI
+*  appropriately. After a sign-in, the API is called.
+*/
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listUpcomingEvents();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+*  Sign in the user upon button click.
+*/
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+*  Sign out the user upon button click.
+*/
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+* Append a pre element to the body containing the given message
+* as its text node. Used to display the results of the API call.
+*
+* @param {string} message Text to be placed in pre element.
+*/
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+* Print the summary and start datetime/date of the next ten events in
+* the authorized user's calendar. If no events are found an
+* appropriate message is printed.
+*/
+function listUpcomingEvents() {
+  gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime'
+  }).then(function(response) {
+    var events = response.result.items;
+    appendPre('Upcoming events:');
+
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
     }
-  };
-
-  const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    res.on('end', () => {
-      console.log('No more data in response.');
-    });
   });
-
-  req.on('error', (e) => {
-    console.log(`problem with request: ${e.message}`);
-  });
-
-  req.end();
-
 }
 
-const api = {
-  publicHolidays
-}
-
-module.exports = api
+export default api
