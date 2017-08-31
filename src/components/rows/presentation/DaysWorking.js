80fb1c435cc4a09e78690f8dea2631b613ab0ed3
hikurangi <hikurangi@gmail.com>
2017-09-01 01:01:50 +1200

    working in place, need to squash errors

--- src/components/rows/presentation/DaysWorking.js
+++ src/components/rows/presentation/DaysWorking.js
@@ -2,13 +2,22 @@ import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-const DaysWorking = ({ daysWorking }) => {
+const DaysWorking = ({ annualLeave, sickLeave, publicHolidays, weekends, days, style }) => {
+  const total = Object.values(days).reduce((a, b) => {
+      if (typeof a === 'number') {
+        return a + b
+      } else {
+        return b
+      }
+    }, 0) - (+annualLeave + +sickLeave + +publicHolidays + +weekends)
   return (
     <TableRow>
       <TableRowColumn>
         <h3>Total number of days you are working this year</h3>
       </TableRowColumn>
-      <TableRowColumn>{ daysWorking }</TableRowColumn>
+      <TableRowColumn>
+        <p style={style.outputNumbers}>{ !isNaN(total) && total }</p>
+      </TableRowColumn>
     </TableRow>
   )
 }
