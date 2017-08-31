cad46844dd8fc6a7c8389d32bd9759effb090140
hikurangi <hikurangi@gmail.com>
2017-09-01 00:34:36 +1200

    data formatted and output correctly

--- src/components/rows/presentation/DaysNotWorking.js
+++ src/components/rows/presentation/DaysNotWorking.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
-const DaysNotWorking = ({ daysNotWorking }) => {
+const DaysNotWorking = ({ annualLeave, sickLeave, publicHolidays, weekends, style }) => {
+  const total = +annualLeave + +sickLeave + +publicHolidays + +weekends
   return (
     <TableRow>
       <TableRowColumn>
         <h3>Total days you are NOT working</h3>
       </TableRowColumn>
-      <TableRowColumn>{ daysNotWorking }</TableRowColumn>
+      <TableRowColumn>
+        <p style={style.outputNumbers}>{ !isNaN(total) && total }</p>
+      </TableRowColumn>
     </TableRow>
   )
 }
