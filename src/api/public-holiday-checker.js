c3bb89d2f7f04e29a93037600cddc51d7d5989ee
hikurangi <hikurangi@gmail.com>
2017-08-31 23:37:57 +1200

    remove console logstep

--- src/api/public-holiday-checker.js
+++ src/api/public-holiday-checker.js
@@ -1,6 +1,6 @@
 const publicHolidayChecker = (country, year) => {
   return fetch(`https://www.kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year=${year}&country=${country}`)
-    .then(res => res.json().length)
+    .then(res => res.json())
     .catch(err => console.log({ err }))
   }
 
