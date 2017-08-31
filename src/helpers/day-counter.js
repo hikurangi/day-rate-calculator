84fe4f2cfc8897a4aeb9d8e5e52ee493ca8d825f
hikurangi <hikurangi@gmail.com>
2017-08-31 12:16:16 +1200

    workday counter

---
+++ src/helpers/day-counter.js
@@ -0,0 +1,23 @@
+// const countWorkdays = inputDays => inputDays.reduce((counter, day) => {
+//     counter[day] ? counter[day]++ : counter[day] = 1
+//     return counter // returns an object with the number of days
+//   }, {})
+
+const getBusinessDatesCount = (startDate, endDate) => {
+    let count = 0;
+    let curDate = startDate;
+    while (curDate <= endDate) {
+        let dayOfWeek = curDate.getDay();
+        if(!((dayOfWeek == 6) || (dayOfWeek == 0)))
+           count++;
+        curDate.setDate(curDate.getDate() + 1);
+    }
+    return count;
+}
+
+const now = new Date()
+const thisYear = now.getFullYear()
+const startDate = new Date(`01/01/${thisYear}`)
+const endDate = new Date(`12/31/${thisYear}`)
+
+export default getBusinessDatesCount(startDate,endDate)
