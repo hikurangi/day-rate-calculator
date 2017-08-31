35a64f2ccca2779862ec03bd5a3bec8b3916b5d2
hikurangi <hikurangi@gmail.com>
2017-08-31 16:37:04 +1200

    add explanatory comment

--- src/helpers/day-counter.js
+++ src/helpers/day-counter.js
@@ -7,7 +7,6 @@ const countDays = (startDate, endDate) => {
     yearInDays.push(currentDate.getDay())
     currentDate = currentDate.setDate(currentDate.getDate() + 1)
   }
-
   return yearInDays.reduce((counter, day) => {
       counter[day] ? counter[day]++ : counter[day] = 1
       return counter
@@ -18,4 +17,4 @@ const now = new Date()
 const thisYear = now.getFullYear()
 const [startDate, endDate] = [new Date(`01/01/${thisYear}`), new Date(`12/31/${thisYear}`)]
 
-export default countDays(startDate, endDate) // returns an object with keys corresponding to each day of the week, and respective values which count how many of those occur during the specified date range, in our case thisYear.
+export default countDays(startDate, endDate) // returns an object with keys corresponding to each day of the week 0-6 where 0=Sunday, and respective values which count how many of those occur during the specified date range, in our case thisYear.
