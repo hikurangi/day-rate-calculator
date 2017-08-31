feb90911ca04b17971adaa82b870f7be717fd8a4
hikurangi <hikurangi@gmail.com>
2017-08-31 23:29:38 +1200

    I think it works

--- src/api/public-holiday-checker.js
+++ src/api/public-holiday-checker.js
@@ -1,11 +1,7 @@
 const publicHolidayChecker = (country, year) => {
-  let output
-  fetch(`https://www.kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year=${year}&country=${country}`) // need to format
-    .then(res => res.json())
-    .then(json => output = json)
-    })
+  return fetch(`https://www.kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year=${year}&country=${country}`)
+    .then(res => res.json().length)
     .catch(err => console.log({ err }))
-  return output
-}
+  }
 
 export default publicHolidayChecker
