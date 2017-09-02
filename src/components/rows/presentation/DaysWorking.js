d38815563a12df75abcf397e3f353ad468c1e240
hikurangi <hikurangi@gmail.com>
2017-09-02 23:46:36 +1200

    simplify prerender logic

--- src/components/rows/presentation/DaysWorking.js
+++ src/components/rows/presentation/DaysWorking.js
@@ -3,13 +3,11 @@ import React from 'react'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
 const DaysWorking = ({ annualLeave, sickLeave, publicHolidays, weekends, days, style }) => {
-  const total = Object.values(days).reduce((a, b) => {
-      if (typeof a === 'number') {
-        return a + b
-      } else {
-        return b
-      }
-    }, 0) - (+annualLeave + +sickLeave + +publicHolidays + +weekends)
+  delete days.thisYear
+  // get total number of days in the year.
+  // subtract weekends, sickLeave, annualLeave, publicHolidays from it.
+  const total = Object.values(days)
+
   return (
     <TableRow>
       <TableRowColumn>
