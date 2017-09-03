9a6076385b6febefb4835dcce5257715f0c105d1
hikurangi <hikurangi@gmail.com>
2017-09-03 16:18:53 +1200

    notes

--- src/components/rows/presentation/DaysWorking.js
+++ src/components/rows/presentation/DaysWorking.js
@@ -4,7 +4,7 @@ import { TableRow, TableRowColumn } from 'material-ui/Table'
 
 const DaysWorking = ({ annualLeave, sickLeave, publicHolidays, weekends, days, style }) => {
   const total = Object.values(days)
-    .filter(value => value < 54) // get rid of the thisYear value
+    .filter(value => value < 54) // get rid of the 'thisYear' value
     .reduce((a, b) => a + b) // add up all the remaining values
     - (annualLeave + sickLeave + publicHolidays + weekends)
   return (
