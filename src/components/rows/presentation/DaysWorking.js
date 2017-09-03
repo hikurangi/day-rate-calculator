9170a32fe7b6bf047eb342aadf5fbc7e23144ff5
hikurangi <hikurangi@gmail.com>
2017-09-03 15:25:41 +1200

    add functional logic for total value

--- src/components/rows/presentation/DaysWorking.js
+++ src/components/rows/presentation/DaysWorking.js
@@ -3,18 +3,17 @@ import React from 'react'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
 const DaysWorking = ({ annualLeave, sickLeave, publicHolidays, weekends, days, style }) => {
-  delete days.thisYear
-  // get total number of days in the year.
-  // subtract weekends, sickLeave, annualLeave, publicHolidays from it.
   const total = Object.values(days)
-
+    .filter(value => value < 54) // get rid of the thisYear value
+    .reduce((a, b) => a + b) // add up all the remaining values
+    - (annualLeave + sickLeave + publicHolidays + weekends)
   return (
     <TableRow>
       <TableRowColumn>
         <h3>Total number of days you are working this year</h3>
       </TableRowColumn>
       <TableRowColumn>
-        <p style={style.outputNumbers}>{ !isNaN(total) && total }</p>
+        <p style={style.outputNumbers}>{ !isNaN(total) && annualLeave && sickLeave && total }</p>
       </TableRowColumn>
     </TableRow>
   )
