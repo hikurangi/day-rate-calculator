453e8d9a592252af3feb20400fb8c49164521e85
hikurangi <hikurangi@gmail.com>
2018-05-19 16:52:51 +1200

    hyperscript refactor

--- src/components/rows/input/FTE.js
+++ src/components/rows/input/FTE.js
@@ -1,26 +1,31 @@
-import React from 'react'
+// import React                        from 'react'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
-import TextField from 'material-ui/TextField'
+import TextField                    from 'material-ui/TextField'
+import { h, a, h3 }              from 'react-hyperscript-helpers'
 
-const FTE = ({ handleChange }) => {
-  return (
-    <TableRow>
-      <TableRowColumn>
-        <h3>
-          Your FTE - <a href="http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf" target="_blank">Full Time Equivalent</a>
-        </h3>
-      </TableRowColumn>
-      <TableRowColumn>
-        <TextField
-          name="fte"
-          type="number"
-          hintText="Generally '1' - meaning 40 hours/week"
-          onChange={handleChange}
-          fullWidth={true}
-        />
-      </TableRowColumn>
-    </TableRow>
-  )
-}
+const FTE = ({ handleChange }) => 
+h(TableRow, [
+  h(TableRowColumn, [
+    h3([
+      'Your FTE',
+      a(
+        {
+          href: "http://centraltas.co.nz/assets/SWS/HWIP/A-practical-guide-to-FTE-reporting-2015.pdf",
+          target: "_blank"
+        },
+        'Full Time Equivalent'
+      )
+    ])
+  ]),
+  h(TableRowColumn, [
+    h(TextField, {
+      name:"fte",
+      type:"number",
+      hintText:"Generally '1' - meaning 40 hours/week",
+      onChange: handleChange,
+      fullWidth: true
+    })
+  ])
+])
 
 export default FTE
